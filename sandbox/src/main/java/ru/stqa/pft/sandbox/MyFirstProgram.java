08c5190ad2bdc1c8b4e0e0cd6b165e1dd07a13f8
Kr1sPo <potapovakristin@gmail.com>
2022-11-09 10:01:16 +0300

    Задание 2 и 3. Функции, классы, объекты и методы. Первый тест.
    Задание2. Потренироваться использовать функции, классы, объекты и методы
    1. Создать класс Point для представления точек на двумерной плоскости. Объекты этого класса должны содержать два атрибута, которые соответствуют координатам точки на плоскости.
    
    2. Создать функцию
    public static double distance(Point p1, Point p2)
    которая вычисляет расстояние между двумя точками. Для вычисления квадратного корня можно использовать функцию Math.sqrt
    
    3. Сделать запускаемый класс, то есть содержащий функцию
    public static void main(String[] args) {...}
    и при помощи него убедиться, что функция вычисления расстояния между точками действительно работает. Результат вычисления выводить на экран и контролировать визуально.
    
    4. Реализовать то же самое (вычисление расстояния между двумя точками) при помощи метода в классе Point, и добавить в созданный в предыдущем пункте запускаемый класс примеры использования метода вместо ранее созданной функции.
    ___
    Задание №3: Подключить тестовый фреймворк и сделать первые тесты
    Подключить к модулю sandbox тестовый фреймворк TestNG и сделать несколько тестов для класса Point, которые проверяют, что расстояние между точками вычисляется правильно.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,9 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main (String[] args) {
-   System.out.println("Hello,world");
+
+    Point p1 = new Point(2,3);
+    Point p2 = new Point(7,10);
+    System.out.println("Расстояние между точками (" + p1.x + ";" + p1.y + ") и (" + p2.x + ";" + p2.y + ") = " + p1.distance(p2));
   }
 }
