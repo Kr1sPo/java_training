a2066aac978b84a23a950a3ca2860608a15f367c
Kr1sPo <potapovakristin@gmail.com>
2022-11-30 17:38:05 +0300

    Задание №8: Реализовать проверку и обеспечение предусловий в тестах
    Доработала тесты для удаления и модификации групп и контактов, чтобы они предварительно проверяли наличие хотя бы одного элемента, который можно удалить или модифицировать, и если такого элемента нет - предварительно создавали его.

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -42,4 +42,14 @@ public class ContactHelper extends HelperBase {
   public void submitContactModification() {
     click(By.name("update"));
   }
+
+  public void createContact(ContactData contact) {
+    initContactCreation();
+    fillContactForm(contact);
+    submitContactCreation();
+  }
+
+  public boolean isThereAContact() {
+    return isElementPresent(By.name("selected[]"));
+  }
 }
